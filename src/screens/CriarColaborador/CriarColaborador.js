950368588b1116391e63f9dcfccc3d838e35d2eb
tarcizo <tarcizoleitemonteiro@gmail.com>
2023-08-22 20:20:17 -0300

    refatoração da tela de criar colaborador

--- src/screens/CriarColaborador/CriarColaborador.js
+++ src/screens/CriarColaborador/CriarColaborador.js
@@ -15,7 +15,7 @@ import ColaboradorService from "../../services/ColaboradorService";
 
 import './CriarColaborador.css'
 export default class CriarColaborador extends React.Component{
-    
+
 
 
     state = {
@@ -24,28 +24,32 @@ export default class CriarColaborador extends React.Component{
         home: {icon: 'pi pi-home ', url: '/' },
 
         estados: [
-            {nome:'Paraiba'}
+            {nome:'Paraiba'},
+            {nome:'Pernanbuco'}
         ],
         tipos: [
-            {tipo:'DISCENTE'}
+            {tipo:'DISCENTE'},
+            {tipo:'DOSCENTE'}
         ],
+
         estado:{nome:''},
         tipo:{tipo:''},
 
         nome:'',
         endereco:'',
         email:'',
-        cidade:'',           
+        cidade:'',
         matricula:'',
         dataDeNascimento:'',
         linkCurriculo:'',
+
         toast:'',
 
         msgDeErro:'',
-        error:'',
+        errorNome:'',
         errorEmail:''
 
-        
+
     }
 
     constructor(){
@@ -55,69 +59,82 @@ export default class CriarColaborador extends React.Component{
 
     delay = (ms) => {
         return new Promise(resolve => setTimeout(resolve, ms));
-      };
-    
-    
+    };
+
+    // Validar se os campos estão preenchidos corretamente
     validar = () =>{
         let msgError= { severity: 'error', summary: 'Corrija os Erros a Baixo', detail: 'Campos não podem ser nulos' };
 
         let disparo = 0;
-
+        //Pre Validação de Nome
         if(this.state.nome === ''){
             disparo ++;
-            let a = document.getElementById('nome'); 
+            let a = document.getElementById('nome');
             a.classList.add('p-invalid');
-            this.setState({error:'Esse Campo é Obrigatorio'})
+            this.setState({errorNome:'Esse Campo é Obrigatorio'})
+            
         }
+
+        //Pre Validação de Endereço
         if(this.state.endereco === ''){
             disparo ++;
-            let a = document.getElementById('endereco') 
-            a.classList.add('p-invalid')
-        }
-        if(this.state.nome === ''){
-            disparo ++;
-            let a = document.getElementById('nome') 
-            a.classList.add('p-invalid')
-        }
-        if(this.state.email === ''){
-            
-            disparo ++;
-            
-            let a = document.getElementById('email') 
-            a.classList.add('p-invalid')
-        }
-        const regex = /@/;
-        if (!regex.test(this.state.email)) {
-            this.setState({errorEmail:'Esse Campo precisa ser um e-mail'})
-          }
-        if(this.state.cidade === ''){
-            disparo ++;
-            let a = document.getElementById('cidade') 
-            a.classList.add('p-invalid')
-        }
-        if(this.state.matricula === ''){
-            disparo ++;
-            let a = document.getElementById('matricula') 
-            a.classList.add('p-invalid')
-        }
-        if(this.state.dataDeNascimento === ''){
-            disparo ++;
-            let a = document.getElementById('dataNascimento') 
-            a.classList.add('p-invalid')
-        }
-        
-        
-        if(this.state.estado.nome === ''){
-            disparo ++;
-            let a = document.getElementById('seletor-estado') 
-            a.classList.add('p-invalid')
-        }
-        if(this.state.tipo.tipo === ''){
-            disparo ++;
-            let a = document.getElementById('seletor-tipo') 
+            let a = document.getElementById('endereco')
             a.classList.add('p-invalid')
         }
 
+        //Pre Validação de email
+        const regex = /@/;
+        if (!regex.test(this.state.email)) {
+            disparo ++;
+            let a = document.getElementById('email')
+            a.classList.add('p-invalid')
+            //this.setState({ error: { email: 'Esse Campo precisa ser um e-mail' } })
+            this.setState({errorEmail:'Esse Campo precisa ser um e-mail'})
+
+        }
+
+        if(this.state.email === ''){
+            disparo ++;
+            let a = document.getElementById('email')
+            a.classList.add('p-invalid')
+            this.setState({errorEmail:'Esse Campo é Obrigatorio'})
+          
+        }
+
+        //Pre Validação de Cidade
+        if(this.state.cidade === ''){
+            disparo ++;
+            let a = document.getElementById('cidade')
+            a.classList.add('p-invalid')
+        }
+
+        //Pre Validação de Matricula
+        if(this.state.matricula === ''){
+            disparo ++;
+            let a = document.getElementById('matricula')
+            a.classList.add('p-invalid')
+        }
+
+        //Pre Validação de Data de Nascimento
+        if(this.state.dataDeNascimento === ''){
+            disparo ++;
+            let a = document.getElementById('dataNascimento')
+            a.classList.add('p-invalid')
+        }
+
+        //Pre Validação de Estado
+        if(this.state.estado.nome === ''){
+            disparo ++;
+            let a = document.getElementById('seletor-estado')
+            a.classList.add('p-invalid')
+        }
+
+        //Pre Validação de Tipo
+        if(this.state.tipo.tipo === ''){
+            disparo ++;
+            let a = document.getElementById('seletor-tipo')
+            a.classList.add('p-invalid')
+        }
 
         if(disparo !== 0){
             this.state.toast.show(msgError);
@@ -126,25 +143,25 @@ export default class CriarColaborador extends React.Component{
             this.confirm();
         }
 
-        
+
     }
 
     confirm = async () => {
         const a = document.getElementsByClassName('p-button p-component p-confirm-dialog-reject p-button-text')
         confirmDialog({
-          
+
             message: 'Você Realmente quer Criar esse Colaborador?',
             icon: 'pi pi-info-circle',
             acceptClassName: 'p-button-danger',
-            
+
             accept:this.accept,
             reject:this.reject,
-            
+
         });
         await this.delay(10);
         document.getElementsByClassName('p-button-label')[7].textContent = "Sim"
         document.getElementsByClassName('p-button-label')[6].textContent = "Não"
-       
+
     };
 
     accept = () => {
@@ -164,6 +181,7 @@ export default class CriarColaborador extends React.Component{
         const mes = data.getMonth() + 1;
         const ano = data.getFullYear();
 
+        //Formata o mes antes de mandar para o back
         console.log("tamanho do mes", mes.size )
         const dataFormatada = `${dia.toString().padStart(2, '0')}/${mes.toString().padStart(2, '0')}/${ano.toString().padStart(2, '0')}`;
         this.service.creat(
@@ -185,10 +203,10 @@ export default class CriarColaborador extends React.Component{
             // matricula:"2020",
             // dataDeNascimento:"19/01/2001",
             // linkCurriculo:"link"
-        
+
         }
         ).then (async (response) =>{
-            
+
             this.state.toast.show({ severity: 'success', summary: 'Sucesso', detail: 'Colaborador Criado Com Sucesso' });
            await this.delay(2000);
             window.location.href = `/colaboradores`;
@@ -198,48 +216,53 @@ export default class CriarColaborador extends React.Component{
         })
     }
 
-    teste = () =>{       
-      
-        
-        this.state.toast.show({ severity: 'success', summary: 'Sucesso', detail: 'Operação realizada com sucesso!' });
-        
-    }
+    // teste = () =>{
+
+
+    //     this.state.toast.show({ severity: 'success', summary: 'Sucesso', detail: 'Operação realizada com sucesso!' });
+
+    // }
 
     render(){
         return(
             <div className="container">
-                
-                <div className="header">
-                    <Toast ref={(el) => (this.state.toast = el)} />
 
+                <div className="header">
+                    {/* Toast: Usado para mostrar mensagem de alerta  */}
+                    <Toast ref={(el) => (this.state.toast = el)} />
+                    {/* BreadCrumb: Usado para o menu de navegaçao que fica ao lado do bt de salvar */}
                     <div className="header-criar-projeto">
                         <BreadCrumb model={this.state.items} home={this.state.home}></BreadCrumb>
                     </div>
+
                     <div className="bt-salvar">
-                    <ConfirmDialog 
+
+                    {/* Campo de dialogo que aparece para confirmar se deseja salvar  */}
+                    {/* Ele chama a função de validar, caso a validação der ok,apresenta o campo para confirmação e caso confirmado, chama a função de salva */}
+                    <ConfirmDialog
                   acceptClassName="p-button-success"
                   rejectClassName="p-button-danger"
                  acceptLabel="Sim"
                  rejectLabel="Não"/>
                         <Button label="Salvar" severity="warning" raised onClick={this.validar} />
-        
+
                     </div>
                 </div>
-                
+
                 <div className="input-texts">
-                    <div className="input-um">                    
+                    <div className="input-um">
                         <label htmlFor="nome">Nome</label>
                         <InputText id="nome" className="borderColorEdit" type="text"
                          value={this.state.nome}
                         onChange={(e) => { this.setState({nome: e.target.value }) }} />
-
-                    {this.state.error && <span style={{ color: 'red' }}>{this.state.error}</span>}
+                    {/* usado para mostrar a msg de erro, caso tenha */}
+                    {this.state.errorNome && <span style={{ color: 'red' }}>{this.state.errorNome}</span>}
 
                     </div>
                     <div className="input-dois">
                         <label id="endereco-label" htmlFor="endereco">Endereço</label>
-                        <InputText id="endereco" className="borderColorEdit" type="text" 
-                        value= {this.state.endereco} 
+                        <InputText id="endereco" className="borderColorEdit" type="text"
+                        value= {this.state.endereco}
                         onChange={(e) => { this.setState({endereco: e.target.value }) }}/>
                     </div>
                 </div>
@@ -248,37 +271,35 @@ export default class CriarColaborador extends React.Component{
                     <div className="input-um">
                         <label htmlFor="email">E-mail</label>
 
-                        <InputText id="email" className="borderColorEdit" type="text" value= 
-                        {this.state.email} 
+                        <InputText id="email" className="borderColorEdit" type="text" value=
+                        {this.state.email}
                         onChange={(e) => { this.setState({email: e.target.value }) }}/>
+
+                        {/* usado para mostrar a msg de erro, caso tenha */}
                         {this.state.errorEmail && <span style={{ color: 'red' }}>{this.state.errorEmail}</span>}
                     </div>
                     <div className="input-dois">
                         <label htmlFor="cidade">Cidade</label>
-                        <InputText id="cidade" className="borderColorEdit input-cidade" type="text" value= 
-                        {this.state.cidade} 
+                        <InputText id="cidade" className="borderColorEdit input-cidade" type="text" value=
+                        {this.state.cidade}
                         onChange={(e) => { this.setState({cidade: e.target.value }) }}/>
                     </div>
 
-
-                   
-
                     <div className="input-dois">
-                    <Dropdown id="seletor-estado" 
-                        value={this.state.estado} 
-                        onChange={(e) => this.setState({estado: this.estado = e.value})} 
-                        options={this.state.estados} 
-                        optionLabel="nome" 
+                    <Dropdown id="seletor-estado"
+                        value={this.state.estado}
+                        onChange={(e) => this.setState({estado: this.estado = e.value})}
+                        options={this.state.estados}
+                        optionLabel="nome"
                         placeholder="Estado" />
                     </div>
                 </div>
-
 
                 <div className="input-texts">
                     <div className="input-dois">
                         <label  htmlFor="matricula">Matrícula</label>
 
-                        <InputText id="matricula" className="borderColorEdit input-cidade" type="text" 
+                        <InputText id="matricula" className="borderColorEdit input-cidade" type="text"
                         value= {this.state.matricula}
                         onChange={(e) => { this.setState({matricula: e.target.value }) }} />
                     </div>
@@ -286,31 +307,29 @@ export default class CriarColaborador extends React.Component{
                     <div className="input-dois">
                         <label  htmlFor="dataNascimento">Data de Nascimento</label>
 
-                        <InputText id="dataNascimento" className="borderColorEdit input-cidade" type="date" placeholder="Data Nascimento" 
+                        <InputText id="dataNascimento" className="borderColorEdit input-cidade" type="date" placeholder="Data Nascimento"
                         value= {this.state.dataDeNascimento}
                         onChange={(e) => { this.setState({dataDeNascimento: e.target.value }) }}/>
                     </div>
 
                     <div className="input-dois">
-                        <Dropdown id="seletor-tipo" 
-                        value={this.state.tipo} onChange={(e) => this.setState({tipo: this.tipo = e.value})} 
-                        options={this.state.tipos} 
-                        optionLabel="tipo" 
+                        <Dropdown id="seletor-tipo"
+                        value={this.state.tipo} onChange={(e) => this.setState({tipo: this.tipo = e.value})}
+                        options={this.state.tipos}
+                        optionLabel="tipo"
                         placeholder="Tipo" />
                     </div>
                 </div>
-
-
 
                 <div className="input-texts">
                     <div className="input-um">
                         <label  htmlFor="linkCurriculo">Link do Curriculo</label>
 
-                        <InputText id="linkCurriculo" className="borderColorEdit" type="text" 
-                        value= {this.state.linkCurriculo} 
+                        <InputText id="linkCurriculo" className="borderColorEdit" type="text"
+                        value= {this.state.linkCurriculo}
                         onChange={(e) => { this.setState({linkCurriculo: e.target.value }) }}/>
                     </div>
-                    
+
                 </div>
             </div>
 
